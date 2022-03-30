0a35f5f1429c44ecbf590c84d861d79c6f9e32a3
Aldo Franquez <aldo.franquez.1997@gmail.com>
2022-03-30 20:16:03 -0300

    Laid groundwork for app (i.e.: design layouts and activities).

---
+++ app/src/main/java/com/example/supersimplenotes/ViewNote.java
@@ -0,0 +1,14 @@
+package com.example.supersimplenotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_note);
+    }
+}
